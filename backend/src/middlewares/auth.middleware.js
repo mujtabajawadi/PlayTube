812f132de38c460e6b38f5fb968a408a53f9496e
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-08-27 13:21:39 +0500

    Wrote loginUser and logoutUser Controller. Also added an auth middleware to check if user is authenticated.

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request!");
+    }
+
+    const decodedTokenInformation = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedTokenInformation?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      //!NEXT_VIDEO: Discussion here
+      throw new ApiError(401, "Invalid Access Token!");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+      throw new ApiError(401, error?.message || "Invalid access token!")
+  }
+});
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -11,5 +12,14 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/loginUser").post(loginUser)
+
+
+//secure routes
+router.route("/logoutUser").post(verifyJWT,logoutUser)
+
+
+
 
 export default router;
