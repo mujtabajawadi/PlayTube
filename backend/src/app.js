46b79448bd16e1d6d22458a211c3a0cbc47efa16
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-09-02 10:21:15 +0500

    Add controllers and routes for comments, likes, playlists, subscriptions, tweets, and videos

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,13 +18,25 @@ app.use(cookieParser())
 
 
 //importing routes 
-import router from './routes/user.routes.js'
+import commentRouter from './routes/comment.routes.js'
+import likeRouter from './routes/like.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+import tweetRouter from './routes/tweet.routes.js'
+import userRouter from './routes/user.routes.js'
+import videoRouter from './routes/video.routes.js'
 
 
 
 
 //routes declaraion
-app.use('/api/v1/users', router)
+app.use('/api/v1/comments', commentRouter)
+app.use('/api/v1/likes', likeRouter)
+app.use('/api/v1/playlists', playlistRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/tweets', tweetRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/videos', videoRouter)
 
 
 export { app };
