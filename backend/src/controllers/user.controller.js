767bedfa9263e92891b7d301e3466792ba4b2f0b
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-12-24 12:31:50 +0500

    feat(auth): enhance login response cookie options with sameSite and maxAge

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -123,6 +123,8 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
   return res
